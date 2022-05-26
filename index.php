48d53e3f0145f6f5e158a2a3ddd841db87958b9b
Orangel Barrera <obarrerafranco@gmail.com>
2022-05-26 11:13:31 +0200

    Creates 2 new array to save the separartion of data results

--- index.php
+++ index.php
@@ -3,13 +3,26 @@
     # Function to identify the type of data from the file
     Function fileTypeData($fileData)
     {
+        #New Array to save the data of the new files
+        $dataNameArray = [];
+
+        #New Array to save the numbers to sum
+        $dataNumberArray = [];
+
+        #Loop to separate the values and push it into a new array
         foreach($fileData as $row => $data) {
             if (is_numeric($data)) {
-               echo "Is a Number ".$data.'<br />';
+                echo "Is a Number ".$data.'<br />';
+                array_push($dataNumberArray, (int)$data);
             } else {
-               echo "Is a File ".$data.'<br />';
+                echo "Is a File ".$data.'<br />';
+                array_push($dataNameArray, $data);
             }
         }  
+
+        print_r($dataNameArray);
+
+        print_r($dataNumberArray);
     }
     #Function to read the first File
     function fileReader($firstFile)
