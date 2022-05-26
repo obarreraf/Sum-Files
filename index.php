aa4f0ee78851c80b68681556f137cd4459329dbc
Orangel Barrera <obarrerafranco@gmail.com>
2022-05-26 11:03:44 +0200

    New Function to Identify the type of Data from the file

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
 
+    # Function to identify the type of data from the file
+    Function fileTypeData($fileData)
+    {
+        foreach($fileData as $row => $data) {
+            if (is_numeric($data)) {
+               echo "Is a Number ".$data.'<br />';
+            } else {
+               echo "Is a File ".$data.'<br />';
+            }
+        }  
+    }
+    #Function to read the first File
     function fileReader($firstFile)
     {
         # now we open the file and covert it into an Array
-        $fileData = file($firstFile);
-
-
-        print_r($fileData);
-
-      
+        $fileData = file($firstFile, FILE_IGNORE_NEW_LINES);
+        fileTypeData($fileData);
     }
-
 
     fileReader('A.txt');
